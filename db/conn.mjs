fd21968b4ae8bed701593c51ec981c080cae8378
jlounsb1 <jefflounsbury@gmail.com>
2024-07-23 11:21:01 -0400

    set up basic express server, added readme and mongoose connection

---
+++ db/conn.mjs
@@ -0,0 +1,11 @@
+import mongoose from "mongoose"
+import { MongoClient } from "mongodb";
+import dotenv from "dotenv";
+dotenv.config();
+
+let db = mongoose.connect(process.env.ATLAS_URI).then((con) =>{
+  console.log(`connected to ${con.connection?.name}`)
+})
+console.log(db)
+export default db;
+
---
+++ server.mjs
@@ -0,0 +1,23 @@
+import express from "express";
+import dotenv from "dotenv";
+dotenv.config();
+import mongoose from "mongoose";
+
+const PORT = 3000;
+const app = express();
+
+app.use(express.json());
+await mongoose.connect(process.env.ATLAS_URI)
+
+app.get("/", (req, res) =>{
+    res.send("Let's find some Dogs!")
+})
+
+app.use((err, _req, res, next) => {
+    res.status(500).send("Ok now it seems we are lost, not the dogs...");
+  });
+  
+  // Start the Express server
+  app.listen(PORT, () => {
+    console.log(`Server is running on port: ${PORT}`);
+  });
