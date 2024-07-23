c5d31e7e215abb3e597630d64f2a59f961fab7b0
jlounsb1 <jefflounsbury@gmail.com>
2024-07-23 12:01:31 -0400

    added placeholder get route on dogs

--- routes/dogs.mjs
+++ routes/dogs.mjs
@@ -8,6 +8,9 @@ router.get("/", async (req, res) => {
     await res.send("This is where we will list lost dogs.")
 })
 
+router.get("/add", async (req, res) => {
+    await res.send(`This is where there will be a submit lost dog form. Post route already set up.`)
+})
 router.post("/add", async (req, res) =>{
     let dogCollection = await Dog;
     let newLostDog = req.body;
